69bfe3b60a40fdf8cf9f6e0ff26618fca3c7c045
Kygan <kygandao23@gmail.com>
2021-07-12 19:44:52 -0700

    added functionality to write new readme file from

--- Develop/index.js
+++ Develop/index.js
@@ -1,63 +1,62 @@
-//Include packages needed for this application
+function generateReadMe() {
+
+const path = require('path');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const markdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        message: 'What is your username?',
-        name: 'user',
-    },
-    {
-        message: 'What is your GitHub user email?',
-        name: 'email',
-    },
-    {
-        message: 'What is the project title?',
-        name: 'title',
-    },
-    {
-        message: 'Please enter a short description of the project.',
-        name: 'description',
-    },
-    {
-        message: 'How does a user INSTALL this application?',
-        name: 'install',
-    },
-    {
-        message: 'How will a user USE this application?',
-        name: 'usage',
-    },
-    {
-        message: 'What Licenses are required?',
-        name: 'license',
-    },
-    {
-        message: 'What are the guidelines for contribution?',
-        name: 'guidelines',
-    },
-    {
-        message: 'What is the test process?',
-        name: 'tests',
-    },
-];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your username?',
+            name: 'user',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub user email?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What is the project title?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Please enter a short description of the project.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'How does a user INSTALL this application?',
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: 'How will a user USE this application?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'What Licenses are required?',
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'What are the guidelines for contribution?',
+            name: 'guidelines',
+        },
+        {
+            type: 'input',
+            message: 'What is the test process?',
+            name: 'tests',
+        },
+    ])
 
-
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {
-
-    // ask questions
-    inquirer.prompt(questions)
-
-    // assemble answers
-    .then((answer) => {
-        return fs.writeFileSync(path.join (process.cwd(), 'README.md'), generate(answer));
+    .then((answers) => {
+        return fs.writeFileSync(path.join (process.cwd(), 'README.md'), markdown(answers))
     });
 }
-    
-// Function call to initialize app
-init();
+
+generateReadMe();
