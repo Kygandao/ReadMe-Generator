d6fbbc490dbf9505353da9a2f532f4d439ae802c
Kygan <kygandao23@gmail.com>
2021-07-12 20:46:32 -0700

    working on markdown semantics

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -50,14 +50,14 @@ function generateMarkdown(answer) {
 # ${answer.title}
   
 ## Table of Contents:
-  1. [Username](## Username)
-  2. [Email](## Email)
-  3. [Description](## Description)
-  4. [Installation](## Installation)
-  5. [Usage](## Usage)
-  6. [License](## License)
-  7. [Contribution Guidelines](## Guidelines)
-  8. [Testing](## Testing)
+  1. [Username](#Username)
+  2. [Email](#Email)
+  3. [Description](#Description)
+  4. [Installation](#Installation)
+  5. [Usage](#Usage)
+  6. [License](#License)
+  7. [Contribution Guidelines](#Guidelines)
+  8. [Testing](#Testing)
 
 ## Username
 ${answer.user}
